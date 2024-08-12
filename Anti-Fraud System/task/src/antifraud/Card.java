5c1ac83b4f6f36c759e126a055efabfe5525e209
Rinze-CodeGorilla <132885446+Rinze-CodeGorilla@users.noreply.github.com>
2024-08-12 10:37:19 +0200

    Fix Luhn check for check digits with value zero

--- Anti-Fraud System/task/src/antifraud/Card.java
+++ Anti-Fraud System/task/src/antifraud/Card.java
@@ -20,18 +20,16 @@ class Card {
 
     }
 
-    static boolean IsValidLuhn(String number)
-    {
+    static boolean IsValidLuhn(String number) {
         int[] digits = number.chars().map(c -> c - '0').toArray();
         int check_digit = 0;
-        for (int i = digits.length - 2; i >= 0; --i)
-            check_digit +=  switch (i % 2)
-        {
-            case 0  -> digits[i] > 4 ? digits[i] * 2 - 9 : digits[i] * 2;
+        for (int i = digits.length - 2; i >= 0; --i) {
+            check_digit += switch (i % 2) {
+                case 0 -> digits[i] > 4 ? digits[i] * 2 - 9 : digits[i] * 2;
                 case 1 -> digits[i];
-            default -> throw new IllegalStateException("Unexpected value: " + i % 2);
-        };
-
-        return 10 - (check_digit % 10) == digits[digits.length - 1];
+                default -> throw new IllegalStateException("Unexpected value: " + i % 2);
+            };
+        }
+        return (check_digit + digits[digits.length - 1]) % 10 == 0;
     }
 }
